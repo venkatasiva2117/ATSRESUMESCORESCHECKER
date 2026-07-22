62e779d2194dca04a63378a9d4375b77903a224a
venkatasiva2117 <venkatasivakumar32@gmail.com>
2026-07-22 08:18:00 +0530

    Add files via upload

---
+++ BACKEND/AiPoweredResumeJobMatcherApplication.java
@@ -0,0 +1,13 @@
+package com.AI_Powered_Resume_JobMatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPoweredResumeJobMatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPoweredResumeJobMatcherApplication.class, args);
+	}
+
+}
